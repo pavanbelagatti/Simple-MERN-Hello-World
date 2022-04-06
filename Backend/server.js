83280ad63805fd1960fc7a1c6d28dfbd3bbcae0a
pavanbelagatti <pavbelnsb@gmail.com>
2022-04-06 10:43:28 +0530

    First commit

---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+//requiering express and initializing the app:
+const app = require('express')(); 
+//requiering the cors middleware:
+const cors = require('cors');
+
+const PORT = 5001; //we will use port 5001
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://maga:t1PbbwzKrRk4arkH@cluster0.p7eeh.mongodb.net/Cluster0?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.use(cors());//telling express to use the cors middleware
+
+app.get('/', (req,res)=>{ //listen to a get request
+  client.connect(async err => {
+    const collection = client.db("test").collection("devices");
+    //perform actions on the collection object
+    //find everything in the collection and turn it into an array:
+    const data = await collection.find().toArray();
+    
+    //now we turn our data into a string and send it over to the client
+    //remember that our data is an array of objects (in this case only one object) but JSON.stringify turns it into a string 
+    res.send(JSON.stringify(data));
+  });
+})
+
+app.listen(PORT, ()=>{ //listen to the port we chose above
+    //print to the console that the server is listening
+    console.log("listening to port: " + PORT);
+})
